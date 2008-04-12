d7f0e18cfb0f0a86d43954d8a13e7912d9cf061a
Edward Ocampo-Gooding <edward@edwardog.net>
2008-04-12 18:40:57 -0400

    Temporarily widened the default week view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @days = (0..5).map {|i| Date.today - i.days - 2.days}.reverse
+    # @days = (0..5).map {|i| Date.today - i.days - 2.days}.reverse
+    @days = (0..14).map {|i| Date.today - i.days}.reverse
   end
 end
