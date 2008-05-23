72426363182b21324a8f606302533d06dfc62112
Edward Ocampo-Gooding <edward@edwardog.net>
2008-05-23 13:25:40 -0400

    Preliminary Client controller stuff

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,33 @@
 class ClientsController < ApplicationController
+  def index
+    @clients = Client.find(:all)
+  end
+  
+  def new
+    @client = Client.new
+  end
+  
+  def create
+    @client = Client.new(params[:client])
+    
+    if @client.save
+      redirect_to clients_url
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @client = Client.find(params[:id])
+  end
+  
+  def update
+    @client = Client.find(params[:id])
+    
+    if @client.update_attributes(params[:client])
+      redirect_to clients_url
+    else
+      render :action => 'edit'
+    end
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,9 @@
 class Client < ActiveRecord::Base
+  has_many :projects
+  
+  validates_uniqueness_of :name
+  validates_numericality_of :default_rate
+  
   def to_s
     name
   end
