792c5e59126b5643a33591952cc0f83b018aec2a
Edward Ocampo-Gooding <edward@edwardog.net>
2008-08-05 01:08:04 -0400

    Added filtering timeline by a single Task

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,5 +1,13 @@
 class TimeEntriesController < ApplicationController
   def index
-    @days = (0..14).map {|i| Date.today - i.days}.reverse
+    if params[:filter_class]
+      # Get whatever was filtered, turning the string into the Class
+      @filtered = Kernel.const_get(params[:filter_class]).find(params[:id])
+      @days = (@filtered.time_entries.find(:first, :order => 'start_time ASC').start_time.to_date .. Date.today).to_a
+    else
+      # TODO Grab one week since the last TimeEntry till today
+      # TimeEntry.find(:first, :order => 'created_at DESC')
+      @days = (0..14).map {|i| Date.today - i.days}.reverse
+    end
   end
 end
