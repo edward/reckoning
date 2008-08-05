9c0d0c12903900d7bb09b4423d246defe31e5c0d
Edward Ocampo-Gooding <edward@edwardog.net>
2008-08-05 12:17:27 -0400

    Added simple rounding to timeline’s tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,13 @@ module ApplicationHelper
   def seconds_in_fractions_of_hours(seconds)
     hours_fraction = (seconds / 3600 * 100).round / 100.0
   end
+  
+  def round_up_to_nearest_15_minutes(seconds)
+    seconds = Integer(seconds)
+    if seconds % 15.minutes > 0 
+      (seconds / 15.minutes) * 15.minutes + 15.minutes
+    else
+      seconds
+    end
+  end
 end
