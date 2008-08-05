03f5686fa2c8eba29e37d9f8098f3bd673149b9b
Edward Ocampo-Gooding <edward@edwardog.net>
2008-08-05 01:17:58 -0400

    A few tweaks to what will become a filter-helper

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,7 +3,7 @@ class TimeEntriesController < ApplicationController
     if params[:filter_class]
       # Get whatever was filtered, turning the string into the Class
       @filtered = Kernel.const_get(params[:filter_class]).find(params[:id])
-      @days = (@filtered.time_entries.find(:first, :order => 'start_time ASC').start_time.to_date .. Date.today).to_a
+      @days = (@filtered.time_entries.find(:first, :order => 'start_time ASC').start_time.to_date .. (@filtered.time_entries.find(:first, :order => 'start_time DESC').end_time.to_date)).to_a
     else
       # TODO Grab one week since the last TimeEntry till today
       # TimeEntry.find(:first, :order => 'created_at DESC')
